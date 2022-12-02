9f847593fe5bcb6e1240d6c5e172fd161f623b6a
Nacholoff <jose.burgosloez@tiernoparla.es>
2022-12-02 20:50:37 +0100

    Añadimos Metodo Numero Aleatorio

--- CodigoHamming.java
+++ CodigoHamming.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
 public class CodigoHamming {
+    // Metodo que genera el MENSAJE aleatoriamente.
+    public static int generaNumeroAleatorio(int minimo, int maximo) {
+
+        return (int) Math.floor(Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
+
+    }// close method
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
         System.out.println("Inserte la longitud del ARRAY");
         int longitud = sn.nextInt();
         int numeros[] = new int[longitud];
+
 
     }
 }
