5b227c5fcbfeeefee5deb73acd5bcf134bfcc93e
Nacholoff <jose.burgosloez@tiernoparla.es>
2022-12-02 20:39:30 +0100

    Añadimos Scanner, variables y array.

--- CodigoHamming.java
+++ CodigoHamming.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class CodigoHamming {
     public static void main(String[] args) {
-    
-        
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Inserte la longitud del ARRAY");
+        int longitud = sn.nextInt();
+        int numeros[] = new int[longitud];
+
     }
 }
