58080263a83c93e654abf6c2e97d7f5aed1e0b99
Nacholoff <jose.burgosloez@tiernoparla.es>
2022-12-02 20:54:36 +0100

    Añadimos rellenar array, generar numero aleatorio y mostrar MENSAJE

--- CodigoHamming.java
+++ CodigoHamming.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CodigoHamming {
@@ -7,12 +8,21 @@ public class CodigoHamming {
         return (int) Math.floor(Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
 
     }// close method
+
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
         System.out.println("Inserte la longitud del ARRAY");
         int longitud = sn.nextInt();
         int numeros[] = new int[longitud];
 
+        // GENERAMOS EL MENSAJE Y RELLENAMOS EL ARRAY
+        for (int i = 0; i < numeros.length; i++) {
 
-    }
-}
+            numeros[i] = generaNumeroAleatorio(0, 1);
+            // System.out.println("En la posicion " + i + " esta el valor: " + numeros[i]);
+
+        } // close for
+        System.out.println("Este es el Mensaje.");
+        System.out.println(Arrays.toString(numeros));
+    }// close main
+}// close public
